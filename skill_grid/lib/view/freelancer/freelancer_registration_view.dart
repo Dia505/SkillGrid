459cb2378b98357708a7a2c7f28906c6f8380971
Dia505 <dia48261@gmail.com>
2024-12-12 14:42:47 +0545

    Role selection screen - responsive design

---
+++ skill_grid/lib/view/freelancer/freelancer_registration_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FreelancerRegistrationView extends StatefulWidget {
+  const FreelancerRegistrationView({super.key});
+
+  @override
+  State<FreelancerRegistrationView> createState() => _FreelancerRegistrationViewState();
+}
+
+class _FreelancerRegistrationViewState extends State<FreelancerRegistrationView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- skill_grid/lib/view/join_client_freelancer_view.dart
+++ skill_grid/lib/view/join_client_freelancer_view.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:skill_grid/core/common/common_button.dart';
 import 'package:skill_grid/core/common/common_logo.dart';
 import 'package:skill_grid/view/client/client_registration_view.dart';
+import 'package:skill_grid/view/freelancer/freelancer_registration_view.dart';
 import 'package:skill_grid/view/login_screen_view.dart';
 
 class JoinClientFreelancerView extends StatefulWidget {
@@ -14,6 +15,64 @@ class JoinClientFreelancerView extends StatefulWidget {
 }
 
 class _JoinClientFreelancerViewState extends State<JoinClientFreelancerView> {
+  bool _isClientSelected = false;
+  bool _isFreelancerSelected = false;
+
+  Color _clientBorderColor = const Color(0xFF929292);
+  Color _freelancerBorderColor = const Color(0xFF929292);
+  Color _clientCircleBorderColor = const Color(0xFF929292);
+  Color _freelancerCircleBorderColor = const Color(0xFF929292);
+  Color _clientCircleColor = const Color(0xFF322E86);
+  Color _freelancerCircleColor = const Color(0xFF322E86);
+
+  void _toggleClientSelection() {
+    setState(() {
+       _isClientSelected = !_isClientSelected;
+
+      if (_isClientSelected) {
+        _clientBorderColor = const Color(0xFFE7E7FF);
+        _clientCircleBorderColor = const Color(0xFFE7E7FF); 
+        _clientCircleColor = const Color(0xFFE7E7FF); 
+      } 
+      else {
+        _clientBorderColor = const Color(0xFF929292);
+        _clientCircleBorderColor = const Color(0xFF929292); 
+        _clientCircleColor = const Color(0xFF322E86);
+      }
+
+      if (_isClientSelected) {
+        _isFreelancerSelected = false;
+        _freelancerBorderColor = const Color(0xFF929292);
+        _freelancerCircleBorderColor = const Color(0xFF929292); 
+        _freelancerCircleColor = const Color(0xFF322E86);
+      }
+    });
+  }
+
+  void _toggleFreelancerSelection() {
+    setState(() {
+      _isFreelancerSelected = !_isFreelancerSelected;
+
+      if (_isFreelancerSelected) {
+        _freelancerBorderColor = const Color(0xFFE7E7FF);
+        _freelancerCircleBorderColor = const Color(0xFFE7E7FF); 
+        _freelancerCircleColor = const Color(0xFFE7E7FF); 
+      } 
+      else {
+        _freelancerBorderColor = const Color(0xFF929292);
+        _freelancerCircleBorderColor = const Color(0xFF929292); 
+        _freelancerCircleColor = const Color(0xFF322E86);
+      }
+
+      if (_isFreelancerSelected) {
+        _isClientSelected = false;
+        _clientBorderColor = const Color(0xFF929292);
+        _clientCircleBorderColor = const Color(0xFF929292); 
+        _clientCircleColor = const Color(0xFF322E86);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,116 +92,150 @@ class _JoinClientFreelancerViewState extends State<JoinClientFreelancerView> {
                 style: GoogleFonts.caprasimo(
                     color: const Color(0xFFE7E7FF), fontSize: 20)),
 
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: const Color(0xFF929292),
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.circular(13)),
-              width: 330,
-              height: 160,
+            GestureDetector(
+              onTap: _toggleClientSelection,
 
-              child: Card(
-                color: const Color(0xFF322E86),
-                shape: RoundedRectangleBorder(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: _clientBorderColor,
+                      width: 3.0,
+                    ),
                     borderRadius: BorderRadius.circular(13)),
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-                  children: [
-                    Image.asset(
-                      "assets/images/join_as_client.png",
-                      width: 95,
-                      height: 135,
-                    ),
-
-                    SizedBox(
-                      width: 170,
-                      child: Text("I’m a client, hiring for a project",
-                          style: GoogleFonts.inter(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 95),
-                      child: Container(
-                        width: 17,
-                        height: 17,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                                color: const Color(0xFF929292), width: 2.0)),
+                width: 330,
+                height: 160,
+              
+                child: Card(
+                  color: const Color(0xFF322E86),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(13)),
+              
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              
+                    children: [
+                      Image.asset(
+                        "assets/images/join_as_client.png",
+                        width: 95,
+                        height: 135,
                       ),
-                    )
-                  ],
+              
+                      SizedBox(
+                        width: 170,
+                        child: Text("I’m a client, hiring for a project",
+                            style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                      ),
+              
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 95),
+                        child: Container(
+                          width: 17,
+                          height: 17,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: _clientCircleBorderColor, width: 2.0),
+                              color: _clientCircleColor
+                            ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
 
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: const Color(0xFF929292),
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.circular(13)),
-              width: 330,
-              height: 160,
+            GestureDetector(
+              onTap: _toggleFreelancerSelection,
 
-              child: Card(
-                color: const Color(0xFF322E86),
-                shape: RoundedRectangleBorder(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: _freelancerBorderColor,
+                      width: 3.0,
+                    ),
                     borderRadius: BorderRadius.circular(13)),
-
-                child: Row(
-
-                  children: [
-                    Image.asset(
-                      "assets/images/join_as_freelancer.png",
-                      width: 140,
-                      height: 125,
-                    ),
-
-                    SizedBox(
-                      width: 151,
-                      child: Text("I’m a freelancer, looking for work",
-                          style: GoogleFonts.inter(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 95),
-                      child: Container(
-                        width: 17,
-                        height: 17,
-
-                        decoration: BoxDecoration(
+                width: 330,
+                height: 160,
+              
+                child: Card(
+                  color: const Color(0xFF322E86),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(13)),
+              
+                  child: Row(
+              
+                    children: [
+                      Image.asset(
+                        "assets/images/join_as_freelancer.png",
+                        width: 140,
+                        height: 125,
+                      ),
+              
+                      SizedBox(
+                        width: 151,
+                        child: Text("I’m a freelancer, looking for work",
+                            style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                      ),
+              
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 95),
+                        child: Container(
+                          width: 17,
+                          height: 17,
+              
+                          decoration: BoxDecoration(
                             shape: BoxShape.circle,
                             border: Border.all(
-                                color: const Color(0xFF929292), width: 2.0)),
+                              color: _freelancerCircleBorderColor, width: 2.0),
+                            color: _freelancerCircleColor
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
 
             CommonButton(
-                buttonText: "Apply as Client",
-                buttonColor: const Color(0xFFE7E7FF),
-                buttonTextColor: const Color(0xFF322E86),
-                onPressed: () {
-                  Navigator.push(
+              buttonText: _isClientSelected
+                ? "Join as Client"
+                : _isFreelancerSelected
+                  ? "Apply as Freelancer"
+                  : "Create Account",
+                  
+              buttonColor: _isClientSelected || _isFreelancerSelected
+                  ? const Color(0xFFE7E7FF) // Active color when a role is selected
+                  : const Color(0xFF929292), // Grey color when no role is selected
+
+              buttonTextColor: _isClientSelected || _isFreelancerSelected
+                  ? const Color(0xFF322E86) // Text color when a role is selected
+                  : const Color(0xFFD3CDCD), // Grey text color when no role is selected
+
+              onPressed: (_isClientSelected || _isFreelancerSelected) ? 
+              () {
+                  if (_isClientSelected) {
+                    Navigator.push(
                       context,
-                      MaterialPageRoute(
-                          builder: (context) => const ClientRegistrationView()));
-                }),
+                      MaterialPageRoute(builder: (context) => const ClientRegistrationView()),
+                    );
+                  } 
+                  else if (_isFreelancerSelected) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const FreelancerRegistrationView()),
+                    );
+                  }
+                }
+              : (){}
+            ),
 
             Padding(
               padding: const EdgeInsets.only(bottom: 70, left: 85),
