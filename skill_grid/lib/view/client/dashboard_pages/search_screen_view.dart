f8bffb704549d44fc13fb5c841f3575f3bae0283
Dia505 <112367545+Dia505@users.noreply.github.com>
2024-12-28 19:12:08 +0545

    Search screen (#5)
    * build profile screen with lottie gif
    
    * build profile progress bar
    
    * job details form view
    
    * freelancer bio form
    
    * bottom navigation bar, themes, fonts
    
    * home screen: app bar with search textfield and button
    
    * Home screen: service category (list of maps)
    
    * Home screen: recently viewed cards (common widget)
    
    * Home screen: popular services (list of maps)
    
    * Home screen: ongoing collaboration card (common)
    
    * Home screen: line divider
    
    * Build your profile: service form + lottie file view timer
    
    * Build your profile: job category screen
    
    * freelancer registration date picker implementation
    
    * Freelancer dashboard: bottom navigation + stats
    
    * Freelancer dashboard: ongoing collabs + recent invoice
    
    * search bar with filter button

--- skill_grid/lib/view/client/dashboard_pages/search_screen_view.dart
+++ skill_grid/lib/view/client/dashboard_pages/search_screen_view.dart
@@ -10,10 +10,64 @@ class SearchScreenView extends StatefulWidget {
 class _SearchScreenViewState extends State<SearchScreenView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Search Screen"),
-      ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.only(top: 20, left: 14),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 60,
+                  width: 314,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: const TextStyle(fontSize: 15),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 30,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                            color: Color(0XFF707070), width: 2),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                            color: Color(0xFF707070), width: 2),
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(
+                        side: BorderSide(
+                          color: Color(0xFF544FBD),
+                          width: 3,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(0),
+                      backgroundColor: const Color(0xFFE7E7FF),
+                      fixedSize: const Size(50, 50)),
+                  child: const Icon(
+                    Icons.tune,
+                    color: Color(0xFF544FBD),
+                    size: 30,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
