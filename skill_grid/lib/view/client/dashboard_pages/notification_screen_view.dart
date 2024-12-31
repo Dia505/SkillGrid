7dfb44f043f2ab3cba6ce11593d7868a7bc0e4dd
Dia505 <112367545+Dia505@users.noreply.github.com>
2024-12-31 23:17:29 +0545

    Client notification screen (#9)
    * build profile screen with lottie gif
    
    * build profile progress bar
    
    * job details form view
    
    * freelancer bio form
    
    * bottom navigation bar, themes, fonts
    
    * home screen: app bar with search textfield and button
    
    * Home screen: service category (list of maps)
    
    * Home screen: recently viewed cards (common widget)
    
    * Home screen: popular services (list of maps)
    
    * Home screen: ongoing collaboration card (common)
    
    * Home screen: line divider
    
    * Build your profile: service form + lottie file view timer
    
    * Build your profile: job category screen
    
    * freelancer registration date picker implementation
    
    * Freelancer dashboard: bottom navigation + stats
    
    * Freelancer dashboard: ongoing collabs + recent invoice
    
    * search bar with filter button
    
    * search screen carousel
    
    * Search screen: skills
    
    * Search screen: filter page
    
    * Client notification screen

--- skill_grid/lib/view/client/dashboard_pages/notification_screen_view.dart
+++ skill_grid/lib/view/client/dashboard_pages/notification_screen_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skill_grid/core/common/notification_card.dart';
 
 class NotificationScreenView extends StatefulWidget {
   const NotificationScreenView({super.key});
@@ -10,10 +11,50 @@ class NotificationScreenView extends StatefulWidget {
 class _NotificationScreenViewState extends State<NotificationScreenView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Notification Screen"),
+    return const Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "Notification",
+                  style: TextStyle(fontSize: 24, fontFamily: "Inter SemiBold"),
+                ),
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage:
+                      AssetImage("assets/images/client_profile_img.jpg"),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            NotificationCard(
+              notificationDate: "1 Dec, 2024",
+              freelancerFirstName: "Anjali",
+              freelancerProfileImgPath:
+                  "assets/images/istockphoto-1354842602-612x612.jpg",
+              projectDate: "5 December, 2024",
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            NotificationCard(
+              notificationDate: "27 Nov, 2024",
+              freelancerFirstName: "Rita",
+              freelancerProfileImgPath:
+                  "assets/images/indian_girl_stock_img.jpg",
+              projectDate: "12 December, 2024",
+            ),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
