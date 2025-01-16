1ca134fc103d436d42913d81fd40730c2a6639a0
Dia505 <dia48261@gmail.com>
2025-01-16 15:09:39 +0545

    Client registration: hive queries, data source and repository

---
+++ skill_grid/lib/features/auth/data/data_source/client_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:skill_grid/features/auth/domain/entity/client_entity.dart';
+
+abstract interface class IClientDataSource {
+  Future<void> registerClient(ClientEntity clientEntity);
+  Future<void> deleteClient(String clientId);
+  Future<ClientEntity?> getClientById(String clientId);
+}
---
+++ skill_grid/lib/features/auth/domain/repository/client_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:skill_grid/core/error/failure.dart';
+import 'package:skill_grid/features/auth/domain/entity/client_entity.dart';
+
+abstract interface class IClientRepository {
+  Future<Either<Failure, void>> registerClient(ClientEntity clientEntity);
+  Future<Either<Failure, void>> deleteClient(String clientId);
+  Future<Either<Failure, ClientEntity?>> getClientById(String clientId);
+}
