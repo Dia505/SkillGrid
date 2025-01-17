28407562abd250f634ca6a76610f91d4c622aace
Dia505 <dia48261@gmail.com>
2025-01-17 17:02:10 +0545

    Onboard screen cubit, event, view + di

--- skill_grid/lib/app/app.dart
+++ skill_grid/lib/app/app.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skill_grid/app/di/di.dart';
 import 'package:skill_grid/core/theme/app_theme.dart';
-import 'package:skill_grid/features/auth/presentation/view/client_registration_view.dart';
-import 'package:skill_grid/features/auth/presentation/view/freelancer_registration_view.dart';
-import 'package:skill_grid/features/auth/presentation/view/login_screen_view.dart';
-import 'package:skill_grid/features/auth/presentation/view_model/login/login_bloc.dart';
-import 'package:skill_grid/features/auth/presentation/view_model/sign_up/client/client_bloc.dart';
-import 'package:skill_grid/features/auth/presentation/view_model/sign_up/freelancer/freelancer_bloc.dart';
 import 'package:skill_grid/features/splash_onboard/presentation/view/splash_screen_view.dart';
 import 'package:skill_grid/features/splash_onboard/presentation/view_model/splash_screen/splash_screen_cubit.dart';
 
--- skill_grid/lib/features/splash_onboard/presentation/view_model/onboard_screen/onboard_screen_cubit.dart
+++ skill_grid/lib/features/splash_onboard/presentation/view_model/onboard_screen/onboard_screen_cubit.dart
@@ -1,5 +1,39 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skill_grid/features/auth/presentation/view_model/join_as_client_freelancer/join_as_client_freelancer_cubit.dart';
+import 'package:skill_grid/features/auth/presentation/view_model/login/login_bloc.dart';
 
-// class OnboardScreenCubit extends Cubit<void> {
-//   final 
-// }
+part 'onboard_screen_event.dart';
+
+class OnboardScreenCubit extends Cubit<void> {
+  final LoginBloc _loginBloc;
+  final JoinAsClientFreelancerCubit _asClientFreelancerCubit;
+
+  OnboardScreenCubit({
+    required LoginBloc loginBloc,
+    required JoinAsClientFreelancerCubit asClientFreelancerCubit,
+  })  : _loginBloc = loginBloc,
+        _asClientFreelancerCubit = asClientFreelancerCubit,
+        super(null);
+
+  void navigateToLoginScreen(BuildContext context, Widget destination) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            BlocProvider.value(value: _loginBloc, child: destination),
+      ),
+    );
+  }
+
+  void navigateJoinAsClientFreelancer(BuildContext context, Widget destination) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => BlocProvider.value(
+            value: _asClientFreelancerCubit, child: destination),
+      ),
+    );
+  }
+}
