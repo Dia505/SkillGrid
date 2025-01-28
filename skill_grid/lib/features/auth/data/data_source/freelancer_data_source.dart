e6c86b7906fb5485832afbf309da3ea0c932d915
Dia505 <dia48261@gmail.com>
2025-01-28 17:50:22 +0545

    freelancer update function + register function returns id

--- skill_grid/lib/features/auth/data/data_source/freelancer_data_source.dart
+++ skill_grid/lib/features/auth/data/data_source/freelancer_data_source.dart
@@ -1,9 +1,10 @@
 import 'package:skill_grid/features/auth/domain/entity/freelancer_entity.dart';
 
 abstract interface class IFreelancerDataSource {
-  Future<void> registerFreelancer(FreelancerEntity freelancerEntity);
+  Future<String> registerFreelancer(FreelancerEntity freelancerEntity);
   Future<void> deleteFreelancer(String freelancerId);
   Future<FreelancerEntity> getFreelancerById(String freelancerId);
   Future<List<FreelancerEntity>> getAllFreelancers();
   Future<String> loginFreelancer(String email, String password);
+  Future<void> updateFreelancer(FreelancerEntity freelancerEntity);
 }
