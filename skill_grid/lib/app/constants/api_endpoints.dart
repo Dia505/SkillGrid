dec32ef23751e60d75d4e3c8f41a0e79ab994cd1
Dia505 <dia48261@gmail.com>
2025-01-28 18:22:27 +0545

    Dio set up + api endpoints + api service

---
+++ skill_grid/lib/app/constants/api_endpoints.dart
@@ -0,0 +1,11 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
+
+  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+
+  //Client routes
+  static const String registerClient = "client";
+}
