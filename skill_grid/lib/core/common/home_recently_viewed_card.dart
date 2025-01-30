e3acdec69b13c7b819a61ca970fe8d37bc8f8757
Dia505 <dia48261@gmail.com>
2025-01-30 14:29:51 +0545

    client dashboard side bar

--- skill_grid/lib/core/common/home_recently_viewed_card.dart
+++ skill_grid/lib/core/common/home_recently_viewed_card.dart
@@ -29,28 +29,28 @@ class HomeRecentlyViewedCard extends StatelessWidget {
         elevation: 3,
         color: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-
         child: Column(
           children: [
             ClipRRect(
               borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15)
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+              child: Image.asset(
+                portfolioImgPath,
+                width: double.infinity,
+                height: 87,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
               ),
-              child: Image.asset(portfolioImgPath,
-                  width: double.infinity, height: 87, fit: BoxFit.cover, alignment: Alignment.topCenter,),
             ),
-
-            const SizedBox(height: 10,),
-                
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.start,
-              
               children: [
                 CircleAvatar(
-                  backgroundImage: AssetImage(freelancerProfileImgPath)),
-
+                    backgroundImage: AssetImage(freelancerProfileImgPath)),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -59,13 +59,13 @@ class HomeRecentlyViewedCard extends StatelessWidget {
                       style: const TextStyle(fontSize: 15),
                     ),
                     Text(profession,
-                        style:
-                            const TextStyle(color: Color(0xFF707070), fontSize: 11)),
+                        style: const TextStyle(
+                            color: Color(0xFF707070), fontSize: 11)),
                     Text(rating, style: const TextStyle(fontSize: 11)),
-                    Text("From Rs.$hourlyRate/hr", style: const TextStyle(fontSize: 11)),
+                    Text("From Rs.$hourlyRate/hr",
+                        style: const TextStyle(fontSize: 11)),
                   ],
                 ),
-
                 Padding(
                   padding: const EdgeInsets.only(top: 45.0),
                   child: SizedBox(
@@ -76,7 +76,10 @@ class HomeRecentlyViewedCard extends StatelessWidget {
                       style: ElevatedButton.styleFrom(
                           backgroundColor: const Color(0XFF544FBD),
                           padding: EdgeInsets.zero),
-                      child: const Icon(Icons.arrow_forward_ios_rounded),
+                      child: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 )
--- skill_grid/lib/features/home/presentation/view/client/client_dashboard.dart
+++ skill_grid/lib/features/home/presentation/view/client/client_dashboard.dart
@@ -16,35 +16,31 @@ class ClientDashboard extends StatelessWidget {
       bottomNavigationBar:
           BlocBuilder<ClientDashboardCubit, ClientDashboardState>(
               builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 18, left: 20, right: 20),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(19),
-            child: BottomNavigationBar(
-                elevation: 0,
-                type: BottomNavigationBarType.fixed,
-                items: const [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.dashboard_rounded), label: "Dashboard"),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.search_rounded), label: "Search"),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.calendar_month_rounded),
-                      label: "Calendar"),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.notifications), label: "Notification"),
-                ],
-                backgroundColor: Colors.white,
-                selectedItemColor: const Color(0xFF544FBD),
-                unselectedItemColor: Colors.black,
-                iconSize: 30,
-                selectedFontSize: 13,
-                unselectedFontSize: 13,
-                currentIndex: state.selectedIndex,
-                onTap: (index) {
-                    context.read<ClientDashboardCubit>().onTabTapped(index);
-                  }),
-          ),
+        return ClipRRect(
+          child: BottomNavigationBar(
+              elevation: 0,
+              type: BottomNavigationBarType.fixed,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.dashboard_rounded), label: "Dashboard"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search_rounded), label: "Search"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.calendar_month_rounded),
+                    label: "Calendar"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.notifications), label: "Notification"),
+              ],
+              backgroundColor: Colors.white,
+              selectedItemColor: const Color(0xFF544FBD),
+              unselectedItemColor: Colors.black,
+              iconSize: 30,
+              selectedFontSize: 13,
+              unselectedFontSize: 13,
+              currentIndex: state.selectedIndex,
+              onTap: (index) {
+                context.read<ClientDashboardCubit>().onTabTapped(index);
+              }),
         );
       }),
     );
