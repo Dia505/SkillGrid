3ba90a5ce5bb0c4db28af5fc81d8aec4ee34648a
Dia505 <112367545+Dia505@users.noreply.github.com>
2025-02-07 15:24:30 +0545

    Testing: unit test for get client by id usecase
    * build profile screen with lottie gif
    
    * build profile progress bar
    
    * job details form view
    
    * freelancer bio form
    
    * bottom navigation bar, themes, fonts
    
    * home screen: app bar with search textfield and button
    
    * Home screen: service category (list of maps)
    
    * Home screen: recently viewed cards (common widget)
    
    * Home screen: popular services (list of maps)
    
    * Home screen: ongoing collaboration card (common)
    
    * Home screen: line divider
    
    * Build your profile: service form + lottie file view timer
    
    * Build your profile: job category screen
    
    * freelancer registration date picker implementation
    
    * Freelancer dashboard: bottom navigation + stats
    
    * Freelancer dashboard: ongoing collabs + recent invoice
    
    * search bar with filter button
    
    * search screen carousel
    
    * Search screen: skills
    
    * Search screen: filter page
    
    * Client notification screen
    
    * Freelancer notification screen + notification card layout changes
    
    * freelancer profile screen: bio data
    
    * folder structuring
    
    * Freelancer profile: services
    
    * Freelancer profile: review container
    
    * Freelancer profile: review pages
    
    * Freelancer profile: skills
    
    * Freelancer profile: education
    
    * Freelancer profile: employment
    
    * clean architecture folder and files added
    
    * Client registration: client and role entity, client hive model, adapter and failure class
    
    * Client registration: hive queries, data source and repository
    
    * Client registration: use case + non-nullable return type for getClientById
    
    * Client registration: bloc + common snackbar
    
    * Client registration: view + dependency injection
    
    * Freelancer registration: entity, hive model, hive queries
    
    * Freelancer registration: data source + repository
    
    * Freelancer registration: usecase, bloc, view + di
    
    * Registration: role assignment in entity, hive model and hive queries
    
    * login functions in data source and repository
    
    * Login: usecase
    
    * login bloc, join as client/freelancer bloc and view
    
    * dependency injection + navigation using bloc
    
    * splash screen cubit + view + di
    
    * Onboard screen cubit, event, view + di
    
    * join as client/freelancer screen navigation (multi bloc provider)
    
    * Login without role-based navigation
    
    * freelancer dashboard cubit, state + di
    
    * client dashboard bloc, navigation of login to register, freelancer login done but client login failed
    
    * role-based login complete
    
    * Appointment: send offer ui
    
    * freelancer update function + register function returns id
    
    * Dio set up + api endpoints + api service
    
    * client registration with api integration
    
    * client registration via api completed
    
    * Freelancer registration via api: connection error
    
    * login via api
    
    * Changed available attribute of freelancer to non-nullable
    
    * client dashboard side bar
    
    * Dashboard side bar bloc
    
    * client profile + edit profile ui
    
    * login dto response changed, login successful
    
    * git merge changes
    
    * shared preferences
    
    * get client by id + freelancer registration success
    
    * token helper to decode token, client home screen cubit
    
    * Client home screen: get client by id - null id
    
    * upload image in client registration
    
    * get client by id in sidebar
    
    * get client by id in Client Profile Screen
    
    * get client by id in edit profile screen
    
    * updateProfilePicture code for both data sources
    
    * update client profile picture: repository, use case, bloc and view
    
    * get client by id: authentication & role authorization + profile picture updated
    
    * client profile update authentication & role authorization + navigation back to client proile and dashboard
    
    * Client update
    
    * Unit test: client registration and login usecase + removed upload picture functionality
    
    * unit test for delete client by id usecase + remote implementation
    
    * unit test for get client by id usecase

---
+++ skill_grid/test/features/auth/domain/use_case/client_use_case/get_client_by_id_use_case_test.dart
@@ -0,0 +1,78 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:skill_grid/core/error/failure.dart';
+import 'package:skill_grid/features/auth/domain/entity/client_entity.dart';
+import 'package:skill_grid/features/auth/domain/use_case/client_use_case/get_client_by_id_use_case.dart';
+
+import '../mock_token_helper.dart';
+import '../mock_token_shared_prefs.dart';
+import 'mock_client_repository.dart';
+
+void main() {
+  late MockClientRepository repository;
+  late MockTokenSharedPrefs tokenSharedPrefs;
+  late MockTokenHelper tokenHelper;
+  late GetClientByIdUseCase useCase;
+
+  setUp(() {
+    repository = MockClientRepository();
+    tokenSharedPrefs = MockTokenSharedPrefs();
+    tokenHelper = MockTokenHelper();
+    useCase = GetClientByIdUseCase(
+        clientRepository: repository,
+        tokenSharedPrefs: tokenSharedPrefs,
+        tokenHelper: tokenHelper);
+  });
+
+  const tClient = ClientEntity(
+      clientId: "1",
+      firstName: "Test",
+      lastName: "Name",
+      mobileNo: "123456",
+      city: "Kathmandu",
+      email: "test@gmail.com",
+      password: "test123",
+      profilePicture: "default_profile_img.png",
+      role: "client");
+
+  const token = "token";
+  const role = "client";
+  const tClientId = "1";
+  final getClientByIdParams = GetClientByIdParams(clientId: tClientId);
+
+  test("Should get client of [clientId: 1] from repository", () async {
+    when(() => tokenSharedPrefs.getToken()).thenAnswer((_) async => const Right(token));
+    when(() => tokenHelper.getRoleFromToken()).thenAnswer((_) async => role);
+    when(() => repository.getClientById(any(), any()))
+        .thenAnswer((_) async => const Right(tClient));
+
+    final result = await useCase(getClientByIdParams);
+
+    expect(result, const Right(tClient));
+
+    verify(() => tokenSharedPrefs.getToken()).called(1);
+    verify(() => tokenHelper.getRoleFromToken()).called(1);
+    verify(() => repository.getClientById(tClientId, token)).called(1);
+
+    verifyNoMoreInteractions(repository);
+    verifyNoMoreInteractions(tokenSharedPrefs);
+    verifyNoMoreInteractions(tokenHelper);
+  });
+
+  test("Should return RoleMismatchFailure when role is not client", () async {
+    when(() => tokenSharedPrefs.getToken()).thenAnswer((_) async => const Right(token));
+    when(() => tokenHelper.getRoleFromToken()).thenAnswer((_) async => "freelancer");
+
+    final result = await useCase(getClientByIdParams);
+
+    expect(result, const Left(RoleMismatchFailure(message: "Access denied. You have to be a client")));
+
+    verify(() => tokenSharedPrefs.getToken()).called(1);
+    verify(() => tokenHelper.getRoleFromToken()).called(1);
+
+    verifyNoMoreInteractions(repository);
+    verifyNoMoreInteractions(tokenSharedPrefs);
+    verifyNoMoreInteractions(tokenHelper);
+  });
+}
